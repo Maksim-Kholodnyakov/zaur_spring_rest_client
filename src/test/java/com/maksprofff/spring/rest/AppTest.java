85333cdb6ac951904fbc4e9c890bb1ba399bdd74
Maksim Kholodnyakov <maksprofff@yandex.ru>
2022-12-26 01:41:23 +0300

    6.13_Написание_REST_Client_commit

--- src/test/java/com/maksprofff/spring/rest/AppTest.java
+++ src/test/java/com/maksprofff/spring/rest/AppTest.java
@@ -1,20 +1,30 @@
 package com.maksprofff.spring.rest;
 
-import static org.junit.Assert.assertTrue;
+import com.maksprofff.spring.rest.configuration.MyConfig;
+import com.maksprofff.spring.rest.entity.Employee;
 
-import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+import java.util.List;
+
+public class AppTest {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication =
+                context.getBean("communication", Communication.class);
+//        List<Employee> allEmp =
+//                communication.getAllEmployee();
+//        System.out.println(allEmp);
+
+//        Employee employee = communication.getEmployee(4);
+//        System.out.println(employee);
+
+//        Employee employee = new Employee("Test2","Testov2","HR",343);
+//        employee.setId(8);
+//        communication.saveEmployee(employee);
+
+        communication.deleteEmployee(8);
+
     }
 }
